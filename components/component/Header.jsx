3060130914c8b9ead4f92b134bd4140d0faa34d0
jattu8602 <chaurasiyajatin68@gmail.com>
2025-06-17 20:05:40 +0530

    Fix formatting in Header component by removing unnecessary semicolon from comment.

--- components/component/Header.jsx
+++ components/component/Header.jsx
@@ -6,7 +6,7 @@ import { ModeToggle } from './ModeToggle'
 export default function Header() {
   return (
     <div>
-      {/* Header */};
+      {/* Header */}
       <div className="flex items-center justify-between px-4 py-4">
         <h1 className="text-2xl font-bold dark:text-white">Daksh</h1>
         <div className="flex items-center space-x-4">
