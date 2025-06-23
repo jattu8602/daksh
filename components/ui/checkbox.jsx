335543f0034134d15daf037d6c817f995a113e14
PrashantDwivedi9596 <codertruf@gmail.com>
2025-06-23 08:07:34 +0530

    Notes V1.1

---
+++ components/ui/checkbox.jsx
@@ -0,0 +1,18 @@
+'use client'
+
+import * as React from 'react'
+
+export const Checkbox = React.forwardRef(
+  ({ className = '', ...props }, ref) => {
+    return (
+      <input
+        type="checkbox"
+        ref={ref}
+        className={`h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500 ${className}`}
+        {...props}
+      />
+    )
+  }
+)
+
+Checkbox.displayName = 'Checkbox'
