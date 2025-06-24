279a34948f7a5170bc46ddcbf26b8bcb0f3de77b
Nitesh Chourasiya <140261740+jattu8602@users.noreply.github.com>
2025-06-24 10:05:35 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,7 +14,7 @@ export const viewport = {
   initialScale: 1,
   maximumScale: 1,
   userScalable: false,
-  themeColor: '#72717f',
+  themeColor: '#B60D0E',
 }
 
 export const metadata = {
@@ -24,17 +24,17 @@ export const metadata = {
   icons: {
     icon: [
       { url: '/icons/icon-192x192.png', sizes: '192x192', type: 'image/png' },
-      { url: '/icons/icon-512x512.png', sizes: '512x512', type: 'image/png' },
+      { url: '/icons/icon-512x512.png', sizes: '512x512', type: 'image/png' }
     ],
     apple: [
-      { url: '/icons/apple-icon.png', sizes: '180x180', type: 'image/png' },
-    ],
+      { url: '/icons/apple-icon.png', sizes: '180x180', type: 'image/png' }
+    ]
   },
   appleWebApp: {
     capable: true,
     statusBarStyle: 'default',
-    title: 'Daksh',
-  },
+    title: 'Daksh'
+  }
 }
 
 export default function RootLayout({ children }) {
@@ -49,25 +49,15 @@ export default function RootLayout({ children }) {
         <link rel="apple-touch-icon" href="/icons/apple-icon.png" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta
-          name="theme-color"
-          content="#72717f"
-          media="(prefers-color-scheme: light)"
-        />
-        <meta
-          name="theme-color"
-          content="#1a1a1a"
-          media="(prefers-color-scheme: dark)"
-        />
+        <meta name="theme-color" content="#B60D0E" />
         <meta
           name="google-site-verification"
           content="1dmarru9P3gaivQMYG8pD5nVERSVBb7rr6ewslRmEiE"
         />
-        {/* add manifest */}
         <link rel="manifest" href="/manifest.json" />
       </head>
       <body
-        className={`${geist.className} min-h-screen bg-background antialiased`}
+        className={`${geist.className} min-h-screen bg-background pt-[env(safe-area-inset-top)] antialiased`}
       >
         <Providers>
           {children}
